43ac2ab4ec718822965db2f0809ed32dfb986634
eemilhaa <eemil.haapanen@gmail.com>
2022-11-08 14:20:55 +0200

    try breaking lint rules

--- src/varasto.py
+++ src/varasto.py
@@ -11,8 +11,7 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela
-    # tilaa tms.
+    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela # tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
